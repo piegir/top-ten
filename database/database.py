5476ca50bd70502e3a052af30b105388df5479f4
Cindy Pereira <Cindy@MacBook-Pro-de-Pierre-8.local>
2024-01-28 18:19:19 +0100

    Fixing get n themes

--- database/database.py
+++ database/database.py
@@ -1,15 +1,16 @@
 import pandas as pd
 
+from lib.theme import Theme
+
 
 class DataBase:
 
     def __init__(self):
         self.themes = pd.read_csv("database/themes.csv")
 
-    def get_n_themes(
-            self,
-            n: int,
-            excluded_indices: list[int] | None = None) -> pd.DataFrame:
+    def get_n_themes(self,
+                     n: int,
+                     excluded_indices: list[int] | None = None) -> list[Theme]:
         """
         Get some random themes from the database, potentially excluding some themes.
 
@@ -22,4 +23,11 @@ class DataBase:
         else:
             possible_themes = self.themes
         selected_themes = possible_themes.sample(n).sort_index()
-        return selected_themes
+        themes = []
+        for i in range(len(selected_themes)):
+            index = selected_themes.iloc[i].name
+            title = selected_themes.iloc[i].title
+            top1 = selected_themes.iloc[i].top1
+            top10 = selected_themes.iloc[i].top10
+            themes.append(Theme(index, title, top1, top10))
+        return themes
