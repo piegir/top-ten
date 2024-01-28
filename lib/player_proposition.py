27115adbb1b43ec67da4eaff4945670a12daaab6
piegir <pie.gir@orange.fr>
2024-01-28 17:19:42 +0100

    fix validator

--- lib/player_proposition.py
+++ lib/player_proposition.py
@@ -5,15 +5,15 @@ from lib.player import Player
 
 class PlayerProposition(BaseModel):
     player: Player = Field(description="The player that made the proposition.",
-                           example=Player())
+                           example=Player(name="John Doe", id=0))
     number: int = Field(
         description="The 'top' number of the player, between 1 and 10.",
         example=5)
     proposition: str | None = Field(
         description="The content of the proposition made.", default=None)
 
-    @field_validator("number")
-    def validate_number(self, number):
-        if not 1 <= number <= 10:
+    @field_validator('number')
+    def check_number_in_1_10(cls, v: int):
+        if not 1 <= v <= 10:
             raise ValueError("'Top' number should be between 1 and 10.")
-        return number
+        return v
