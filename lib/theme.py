ebbd68d6e396b761b590c4042d1007801eb2eeb3
Cindy Pereira <Cindy@MacBook-Pro-de-Pierre-8.local>
2024-01-28 16:41:13 +0100

    Adding index to player_proposition

--- lib/theme.py
+++ lib/theme.py
@@ -1,9 +1,10 @@
 class Theme:
 
-    def __init__(self):
-        self.title: str = "Mr Bond, si vous ne me donnez pas le code secret, je vais... "
-        self.top1: str = "Plus petite menace"
-        self.top10: str = "Plus grande menace"
+    def __init__(self, index: int, title: str, top1: str, top10: str):
+        self.index: int = index
+        self.title: str = title
+        self.top1: str = top1
+        self.top10: str = top10
 
     def __str__(self) -> str:
         return f"{self.title}\nTop 1 : {self.top1}\nTop 10 : {self.top10}"
