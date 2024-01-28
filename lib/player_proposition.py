a1114624ef5d4e0daeb4132c6c11bca9c545101e
piegir <pie.gir@orange.fr>
2024-01-28 17:09:22 +0100

    make sure classes that will go through rest api are pydantic classes

--- lib/player_proposition.py
+++ lib/player_proposition.py
@@ -1,9 +1,19 @@
+from pydantic import BaseModel, Field, field_validator
+
 from lib.player import Player
 
 
-class PlayerProposition:
+class PlayerProposition(BaseModel):
+    player: Player = Field(description="The player that made the proposition.",
+                           example=Player())
+    number: int = Field(
+        description="The 'top' number of the player, between 1 and 10.",
+        example=5)
+    proposition: str | None = Field(
+        description="The content of the proposition made.", default=None)
 
-    def __init__(self, number: int, player: Player):
-        self.player: Player = player
-        self.number: int = number
-        self.proposition: str | None = None
+    @field_validator("number")
+    def validate_number(self, number):
+        if not 1 <= number <= 10:
+            raise ValueError("'Top' number should be between 1 and 10.")
+        return number
--- lib/theme.py
+++ lib/theme.py
@@ -1,9 +1,19 @@
-class Theme:
+from pydantic import BaseModel, Field
 
-    def __init__(self):
-        self.title: str = "Mr Bond, si vous ne me donnez pas le code secret, je vais... "
-        self.top1: str = "Plus petite menace"
-        self.top10: str = "Plus grande menace"
+
+class Theme(BaseModel):
+    title: str = Field(
+        description="The title of the theme, what it deals with.",
+        example="Mr Bond, si vous ne me donnez pas le code secret, je vais... "
+    )
+    top1: str = Field(
+        description=
+        "Explanation of the top 1 (most 'something') option for that theme.",
+        example="Plus petite menace")
+    top10: str = Field(
+        description=
+        "Explanation of the top 10 (most opposite 'something') option for that theme.",
+        example="Plus grande menace")
 
     def __str__(self) -> str:
         return f"{self.title}\nTop 1 : {self.top1}\nTop 10 : {self.top10}"
