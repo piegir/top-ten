d608a03afeb294fd8d09334450e039fdf1a05cf9
piegir <87880872+piegir@users.noreply.github.com>
2024-02-24 15:19:02 +0100

    Update formatting (#41)
    * added js formatting
    
    * update pre-commit hooks to latest versions for each

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,114 +1,115 @@
 import './App.css';
+
 import {Component} from "react";
-import {currentUser, userLogout, AskCredentials} from "./authentication/authentication.js";
-import {GamePreparation} from "./game/game.js"
+
 import {
-    ThemeSelection,
-    PropositionMaking,
-    HypothesisMaking
-} from "./rounds/rounds.js"
-import {RoundResultChecking, GameResultChecking} from "./results/results.js"
+  AskCredentials,
+  currentUser,
+  userLogout
+} from "./authentication/authentication.js";
 import {wait} from "./common/common.js";
+import {GamePreparation} from "./game/game.js"
+import {GameResultChecking, RoundResultChecking} from "./results/results.js"
+import {
+  HypothesisMaking,
+  PropositionMaking,
+  ThemeSelection
+} from "./rounds/rounds.js"
 
 let views = {
-    AskCredentials: 0,
-    GamePreparation: 1,
-    ThemeSelection: 2,
-    PropositionMaking: 3,
-    HypothesisMaking: 4,
-    RoundResultChecking: 5,
-    GameResultChecking: 6,
+  AskCredentials : 0,
+  GamePreparation : 1,
+  ThemeSelection : 2,
+  PropositionMaking : 3,
+  HypothesisMaking : 4,
+  RoundResultChecking : 5,
+  GameResultChecking : 6,
 }
 
 class App extends Component {
 
-    constructor(props) {
-        super(props);
-        window.onbeforeunload = (event) => {
-            const e = event;
-            // Cancel the event
-            e.preventDefault();
-            if (e) {
-                e.returnValue = ''; // Legacy method for cross browser support
-            }
-            return ''; // Legacy method for cross browser support
-        };
-
-        window.onpagehide = (event) => {
-            userLogout().then();
-            // Wait 1s to make sure the user was logged out.
-            wait(1000);
-        };
-
-    }
-
-    state = {view: views.AskCredentials};
-
-    /// Logic Handlers
-    goToGamePreparationHandler = () => {
-        this.setState({view: views.GamePreparation});
+  constructor(props) {
+    super(props);
+    window.onbeforeunload = (event) => {
+      const e = event;
+      // Cancel the event
+      e.preventDefault();
+      if (e) {
+        e.returnValue = ''; // Legacy method for cross browser support
+      }
+      return ''; // Legacy method for cross browser support
     };
 
-    goToAskCredentialsHandler = () => {
-        this.setState({view: views.AskCredentials});
+    window.onpagehide = (event) => {
+      userLogout().then();
+      // Wait 1s to make sure the user was logged out.
+      wait(1000);
     };
+  }
 
-    goToThemeSelectionHandler = () => {
-        this.setState({view: views.ThemeSelection});
-    };
+  state = {view : views.AskCredentials};
 
-    goToPropositionMakingHandler = () => {
-        this.setState({view: views.PropositionMaking});
-    };
+  /// Logic Handlers
+  goToGamePreparationHandler =
+      () => { this.setState({view : views.GamePreparation}); };
 
-    goToHypothesisMakingHandler = () => {
-        this.setState({view: views.HypothesisMaking});
-    };
+  goToAskCredentialsHandler =
+      () => { this.setState({view : views.AskCredentials}); };
 
-    goToRoundResultsCheckingHandler = () => {
-        this.setState({view: views.RoundResultChecking});
-    };
+  goToThemeSelectionHandler =
+      () => { this.setState({view : views.ThemeSelection}); };
 
-    goToGameResultsCheckingHandler = () => {
-        this.setState({view: views.GameResultChecking});
-    };
+  goToPropositionMakingHandler =
+      () => { this.setState({view : views.PropositionMaking}); };
 
-    render() {
-        switch (this.state.view) {
-            case views.AskCredentials:
-                return (
-                    <AskCredentials goToGamePreparationHandler={this.goToGamePreparationHandler}/>
+  goToHypothesisMakingHandler =
+      () => { this.setState({view : views.HypothesisMaking}); };
+
+  goToRoundResultsCheckingHandler =
+      () => { this.setState({view : views.RoundResultChecking}); };
+
+  goToGameResultsCheckingHandler =
+      () => { this.setState({view : views.GameResultChecking}); };
+
+  render() {
+    switch (this.state.view) {
+    case views.AskCredentials:
+      return (<AskCredentials goToGamePreparationHandler = {
+        this.goToGamePreparationHandler
+      } />
                 )
             case views.GamePreparation:
                 return (
                     <GamePreparation goToThemeSelectionHandler={this.goToThemeSelectionHandler}
-                                     goToAskCredentialsHandler={this.goToAskCredentialsHandler}/>
-                )
-            case views.ThemeSelection:
-                return (
-                    <ThemeSelection goToPropositionMakingHandler={this.goToPropositionMakingHandler}
-                                    goToAskCredentialsHandler={this.goToAskCredentialsHandler}/>
+                                     goToAskCredentialsHandler={this.goToAskCredentialsHandler}/>)
+    case views.ThemeSelection:
+      return (<ThemeSelection goToPropositionMakingHandler = {
+                   this.goToPropositionMakingHandler} goToAskCredentialsHandler = {
+        this.goToAskCredentialsHandler
+      } />
                 )
             case views.PropositionMaking:
                 return (
                     <PropositionMaking goToHypothesisMakingHandler={this.goToHypothesisMakingHandler}
-                                       goToAskCredentialsHandler={this.goToAskCredentialsHandler}/>
-                )
-            case views.HypothesisMaking:
-                return (
-                    <HypothesisMaking goToRoundResultsCheckingHandler={this.goToRoundResultsCheckingHandler}
-                                      goToAskCredentialsHandler={this.goToAskCredentialsHandler}/>
+                                       goToAskCredentialsHandler={this.goToAskCredentialsHandler}/>)
+    case views.HypothesisMaking:
+      return (<HypothesisMaking goToRoundResultsCheckingHandler = {
+                   this.goToRoundResultsCheckingHandler} goToAskCredentialsHandler = {
+        this.goToAskCredentialsHandler
+      } />
                 )
             case views.RoundResultChecking:
                 return (
                     <RoundResultChecking goToThemeSelectionHandler={this.goToThemeSelectionHandler}
                                          goToGameResultsCheckingHandler={this.goToGameResultsCheckingHandler}
-                                         goToAskCredentialsHandler={this.goToAskCredentialsHandler}/>
-                )
-            case views.GameResultChecking:
-                return (
-                    <GameResultChecking goToGamePreparationHandler={this.goToGamePreparationHandler}
-                                        goToAskCredentialsHandler={this.goToAskCredentialsHandler}/>
+                                         goToAskCredentialsHandler={this.goToAskCredentialsHandler}/>)
+    case views.GameResultChecking:
+      return (
+          <GameResultChecking goToGamePreparationHandler =
+               {this.goToGamePreparationHandler} goToAskCredentialsHandler =
+           {
+             this.goToAskCredentialsHandler
+           } />
                 )
             default:
                 return (
@@ -116,11 +117,12 @@ class App extends Component {
                     {currentUser.username !== null ?
                         <GamePreparation goToThemeSelectionHandler={this.goToThemeSelectionHandler}
                                          goToAskCredentialsHandler={this.goToAskCredentialsHandler}/>:
-                        <AskCredentials goToAskCredentialsHandler={this.goToAskCredentialsHandler}/>}
-                    </div>
-                )
-        }
+              <AskCredentials goToAskCredentialsHandler = {
+                this.goToAskCredentialsHandler
+              } />}
+                    </div>)
     }
+  }
 }
 
 export default App;
--- frontend/src/results/results.js
+++ frontend/src/results/results.js
@@ -1,83 +1,79 @@
 import {Component} from "react";
-import {makeGetCall, repeat} from "../common/common.js";
-import {Username} from "../authentication/authentication.js";
-import {GameProgress, GameSummary} from "../game/game_progress.js";
-import {getGamePlayers} from "../game/game.js";
-import {CurrentTheme} from "../rounds/theme_selection.js";
-import {Hypothesis, Reality, RoundResult} from "./round_result.js";
-import {Users} from "../authentication/users";
 
+import {Username} from "../authentication/authentication.js";
+import {Users} from "../authentication/users";
+import {makeGetCall, repeat} from "../common/common.js";
+import {getGamePlayers} from "../game/game.js";
+import {GameProgress, GameSummary} from "../game/game_progress.js";
+import {CurrentTheme} from "../rounds/theme_selection.js";
+
+import {Hypothesis, Reality, RoundResult} from "./round_result.js";
 
 function checkRoundResult() {
-    return makeGetCall("/rounds/check_round_result");
+  return makeGetCall("/rounds/check_round_result");
 }
 
 export class RoundResultChecking extends Component {
 
-    state = {
-        result: null,
-        hypothesis: null,
-        reality: null,
-    }
+  state = {
+    result : null,
+    hypothesis: null,
+    reality: null,
+  }
 
-    componentDidMount() {
-        checkRoundResult().then((roundResult) => {
-            this.setState(roundResult);
-        });
-    }
+  componentDidMount() {
+    checkRoundResult().then((roundResult) => { this.setState(roundResult); });
+  }
 
-    checkRoundStarted = () => {
+  checkRoundStarted =
+      () => {
         makeGetCall("/game/is_round_in_progress").then((inProgress) => {
-            if (inProgress) {
-                this.props.goToThemeSelectionHandler();
-            }
-            else {
-                this.roundStartedCheckingId = repeat(this.checkRoundStarted, 100);
-            }
+          if (inProgress) {
+            this.props.goToThemeSelectionHandler();
+          } else {
+            this.roundStartedCheckingId = repeat(this.checkRoundStarted, 100);
+          }
         })
+      }
+
+  roundStartedCheckingId = repeat(this.checkRoundStarted, 100);
+
+  componentWillUnmount() { clearTimeout(this.roundStartedCheckingId); }
+
+  render() {
+    if (this.state.result === null) {
+      return null;
     }
-
-    roundStartedCheckingId = repeat(this.checkRoundStarted, 100);
-
-    componentWillUnmount() {
-        clearTimeout(this.roundStartedCheckingId);
-    }
-
-    render() {
-        if (this.state.result === null) {
-            return null;
-        }
-        return (
-            <div className="GlobalGrid">
-                <div className="HeadBox">
-                    <div className="Title">
-                        Top Ten
-                    </div>
+    return (
+        <div className = "GlobalGrid"><div className = "HeadBox">
+        <div className = "Title">Top Ten<
+            /div>
                     <Username goToAskCredentialsHandler={this.props.goToAskCredentialsHandler}/>
-                </div>
+        </div>
                 <div className="MiddleBox">
                     <div className="GameProgressBox">
                         <GameProgress/>
-                    </div>
+        </div>
                     <CurrentTheme/>
-                    <RoundResult result={this.state.result}/>
+        <RoundResult result = { this.state.result } />
                 </div>
-                <div className="BottomBox">
-                    <Reality reality={this.state.reality}/>
+        <div className = "BottomBox">
+        <Reality reality =
+         {
+           this.state.reality
+         } />
                     <Hypothesis hypothesis={this.state.hypothesis}
                                 reality={this.state.reality}
                                 goToThemeSelectionHandler={this.props.goToThemeSelectionHandler}
                                 goToGameResultsCheckingHandler={this.props.goToGameResultsCheckingHandler}
                     />
-                </div>
-            </div>
-        );
-    }
+        </div>
+            </div>);
+  }
 }
 
-
 export class GameResultChecking extends Component {
-    render() {
+  render() {
         return (
             <div className="GlobalGrid">
                 <div className="HeadBox">
@@ -92,7 +88,8 @@ export class GameResultChecking extends Component {
                     </div>
                 </div>
                 <div className="BottomBox">
-                    <Users getUsersListHandler={getGamePlayers} checkOnlyOnce={true} displayNumbers={false}/>
+                    <Users getUsersListHandler={getGamePlayers} checkOnlyOnce={true} displayNumbers={
+      false}/>
                     <div className="UserActionBox">
                         <div className="SubTitle">
                             Setup a new game?
