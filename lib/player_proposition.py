b2209e209835c823e604d2ac20ddd6ead5f6f6e6
piegir <pie.gir@orange.fr>
2024-02-24 13:06:16 +0100

    simplify proposition making data structure

--- lib/player_proposition.py
+++ lib/player_proposition.py
@@ -1,17 +1,15 @@
 from pydantic import BaseModel, Field, field_validator
 
 
-class Proposition(BaseModel):
-    proposition: str | None = Field(
-        description="The content of the proposition made.", default=None)
-
-
 class PlayerProposition(BaseModel):
     player: str = Field(
         description="The username of the player that made the proposition.",
         example="johndoe")
     proposition: str | None = Field(
         description="The content of the proposition made.", default=None)
+
+    def __str__(self) -> str:
+        return f"- Player: {self.player}\n- Proposition : {self.proposition}"
 
 
 class NumberedPlayerProposition(BaseModel):
@@ -21,6 +19,9 @@ class NumberedPlayerProposition(BaseModel):
     player_proposition: PlayerProposition = Field(
         description="The player proposition.",
         example=PlayerProposition(player="johndoe"))
+
+    def __str__(self) -> str:
+        return f"{self.player_proposition}\n- Number: {self.number}"
 
     @field_validator('number')
     def check_number_in_1_10(cls, v: int):
