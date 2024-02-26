c9e0c51314fcf07a63286ce8ebadeb4352ef9101
piegir <87880872+piegir@users.noreply.github.com>
2024-02-26 12:04:45 +0100

    English themes (#43)
    * temporary english themes, being manually fixed
    
    * fixed english themes + small fixes in french themes
    
    * fix last themes, integrate usage of english themes in library, API and frontend
    
    * fix callbacks

--- database/database.py
+++ database/database.py
@@ -2,11 +2,23 @@ import pandas as pd
 
 from lib.theme import Theme
 
+supported_languages = ["en", "fr"]
+
+themes = {
+    language: pd.read_csv(f"database/themes_{language}.csv")
+    for language in supported_languages
+}
+
 
 class DataBase:
 
-    def __init__(self):
-        self.themes = pd.read_csv("database/themes.csv")
+    def __init__(self, language="en"):
+        if language not in supported_languages:
+            raise ValueError(
+                f"Provided language must be one of the supported languages: {supported_languages}"
+            )
+        themes_file = f"database/themes_{language}.csv"
+        self.themes = pd.read_csv(themes_file)
 
     def get_n_themes(self,
                      n: int,
@@ -33,16 +45,23 @@ class DataBase:
         ]
         return themes
 
-    def pick_random_card(self, nb_themes_per_card: int,
-                         played_themes: list[Theme]) -> list[Theme]:
-        """
-        Called by the playing player, sets the card to list of nb_themes themes
-        :param nb_themes_per_card: number of themes on the card
-        :param played_themes: themes already played
-        :return: the list of random themes
-        """
-        played_indices = [theme.index for theme in played_themes]
-        return self.get_n_themes(nb_themes_per_card, played_indices)
+
+# Store theme database for each language
+databases = {language: DataBase(language) for language in supported_languages}
 
 
-database = DataBase()
+def pick_random_card(nb_themes_per_card: int, language: str,
+                     played_themes: list[Theme]) -> list[Theme]:
+    """
+    Called by the playing player, sets the card to list of nb_themes themes
+    :param nb_themes_per_card: number of themes on the card
+    :param language: the language to use for the themes on the card
+    :param played_themes: themes already played
+    :return: the list of random themes
+    """
+    if language not in supported_languages:
+        raise ValueError(
+            f"Provided language must be one of the supported languages: {supported_languages}"
+        )
+    played_indices = [theme.index for theme in played_themes]
+    return databases[language].get_n_themes(nb_themes_per_card, played_indices)
